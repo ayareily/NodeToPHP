d9903886fdc2784351924f31de8639daa64cb172
ayareily <musiclear05@gmail.com>
2020-05-28 13:24:40 +0900

    一覧表示機能追加

--- index.php
+++ index.php
@@ -1,6 +1,30 @@
 <?php
     include 'includes/login.php';
 
+    $num = 10;
+
+    $dsn ='mysql:host=localhost;dbname=secret_board;charset=utf8';
+    $user = 'bbsuser';
+    $password = 'password';
+
+    $page = 0;
+    if (isset($_GET['page']) && $_GET['page'] > 0){
+        $page = intval($_GET['page']) -1;
+    }
+
+    try {
+        $db = new PDO($dsn, $user, $password);
+        $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        $stmt = $db->prepare(
+            'SELECT * FROM post ORDER BY date DESC LIMIT :page, :num'
+        );
+        $page = $page * $num;
+        $stmt->bindParam(':page', $page, PDO::PARAM_INT);
+        $stmt->bindParam(':num', $num, PDO::PARAM_INT);
+        $stmt->execute();
+    } catch (PDOException $e){
+        echo 'エラー：' . $e->getMessage();
+    }
 ?>
 
 <html>
@@ -17,5 +41,32 @@
         <p><input type="submit" value="書き込む">
     </form>
     <hr />
+
+<?php
+    while ($row = $stmt->fetch()):
+?>
+    <p>名前：<?php echo $_SESSION['id'] ?></p>
+    <p><?php echo nl2br(htmlspecialchars($row['content'], ENT_QUOTES, 'UTF-8'), false) ?></p>
+    <p><?php echo $row['date'] ?></p>
+    <form action="delete.php" method="post">
+        <input type="hidden" name="id" value="<?php echo $row ['id']; ?>">
+        <input type="hidden" name="token" value="<?php echo sha1(session_id()); ?>">
+    </form>
+<?php
+    endwhile;
+
+    try{
+        $stmt = $db->prepare('SELECT COUNT(*) FROM post');
+        $stmt->execute();
+    }   catch (PDOException $e) {
+        echo 'エラー：' . $e->getMessage();
+    }
+    $max_page = ceil($comments / $num);
+    echo '<p>';
+    for ($i =1; $i <= $max_page; $i++){
+        echo '<a href="bbs.php?page=' . $i . '">' . $i . '</a>&nbsp;';
+    }
+    echo '</p>';
+?>
 </body>
 </html>
--- write.php
+++ write.php
@@ -2,14 +2,16 @@
     include 'includes/login.php';
 
     $content = $_POST['content'];
-    $userid = $_SESSION['userid'];
+    $userid = $_SESSION['id'];
 
-    /*
+    setcookie('trackingid', $trackingid, time() + 60 * 60 * 24 * 30);
+
+    $trackingid = $_COOKIE['trackingid'];
+/*
     if ($token !=sha1(session_id())){
         header('Location: index.php');
         exit();
     }
-    setcookie('userid', $userid, time() + 60 * 60 * 24 * 30);
     */
 
     $dsn = 'mysql:host=localhost;dbname=secret_board;charset=utf8';
@@ -20,11 +22,12 @@
         $db = new PDO($dsn, $user, $password);
         $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         $stmt = $db->prepare("
-        INSERT INTO post (content, user_id, date)
-        VALUES (:content, :userid, now())"
+        INSERT INTO post (content, user_id, date, cookie)
+        VALUES (:content, :userid, now(), :cookie)"
     );
     $stmt->bindParam(':content', $content, PDO::PARAM_STR);
     $stmt->bindParam(':userid', $userid, PDO::PARAM_INT);
+    $stmt->bindParam(':cookie', $trackingid, PDO::PARAM_STR);
     $stmt->execute();
 
     header('Location: index.php');
